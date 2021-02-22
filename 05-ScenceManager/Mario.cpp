ad1ed2b2310dd26c9ed7d183fd19c5e22cb53878
Nguyen Duc <phucducktpm@gmail.com>
2021-02-22 22:21:55 +0700

    add texture, sprites, animations for mario small

--- 05-ScenceManager/Mario.cpp
+++ 05-ScenceManager/Mario.cpp
@@ -150,7 +150,7 @@ void CMario::Render()
 	int alpha = 255;
 	if (untouchable) alpha = 128;
 
-	animation_set->at(ani)->Render(x, y, alpha);
+	animation_set->at(17)->Render(x, y, alpha);
 
 	RenderBoundingBox();
 }
